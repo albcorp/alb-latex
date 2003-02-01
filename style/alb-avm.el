6bb4b2a38ea115467c155db853984f3f14f13263
andrew <devnull@localhost>
2003-02-01 09:22:54 +0000

    Correct for factorization. Whitespace cleanup.

--- style/alb-avm.el
+++ style/alb-avm.el
@@ -1,26 +1,25 @@
-;;; 
-;;; AlbThesis/style/alb-avm.el
-;;; 
+;;;
+;;; AlbAVM/style/alb-avm.el
+;;;
 ;;;     Copyright (C) 1999-2003 Andrew Lincoln Burrow
-;;; 
+;;;
 ;;;     This library is free software; you can redistribute it and/or
 ;;;     modify it under the terms of the GNU General Public License as
 ;;;     published by the Free Software Foundation; either version 2 of
-;;;     the License, or (at your option) any later version. 
-;;; 
+;;;     the License, or (at your option) any later version.
+;;;
 ;;;     This library is distributed in the hope that it will be useful,
 ;;;     but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;;     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;     GNU General Public License for more details. 
-;;; 
+;;;     GNU General Public License for more details.
+;;;
 ;;;     You should have received a copy of the GNU General Public
 ;;;     License along with this library; if not, write to the Free
 ;;;     Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ;;;     MA 02111-1307, USA.
-;;; 
-;;;   - Customise AUCTeX to provide guided recursive construction of typed
-;;;     feature structures in attribute-value matrix (AVM) notation.
-;;; 
+;;;
+;;;   - Customise AUCTeX for AlbAVM LaTeX package.
+;;;
 ;;; $Id$
 ;;;
 
@@ -228,6 +227,6 @@ AUCTeX customisation."
 
 
 
-;;; Local Variables: 
+;;; Local Variables:
 ;;; mode: emacs-lisp
-;;; End: 
+;;; End:
